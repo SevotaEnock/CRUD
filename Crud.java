7d97b88b288b6aa46b3ec12a3b98d0b792c1c2ad
SevotaEnock <129184233+SevotaEnock@users.noreply.github.com>
2023-03-25 11:12:19 +0300

    Adding new profiler

---
+++ Crud.java
@@ -0,0 +1,9 @@
+// create a simpler project to perform the coding task
+import java.util.Scanner;
+public class Crud{
+    // create the main method
+    public static void main(String[] args){
+        // introduce the Scanner method
+        Scanner input = new Scanner(System.in);
+    }
+}
