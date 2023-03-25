cbdf088c295fb4bd7d46ad6e70c2619a051463ce
SevotaEnock <129184233+SevotaEnock@users.noreply.github.com>
2023-03-25 11:19:49 +0300

    Update the new line of code
    Adding new lines of codes in my project

--- Crud.java
+++ Crud.java
@@ -5,5 +5,25 @@ public class Crud{
     public static void main(String[] args){
         // introduce the Scanner method
         Scanner input = new Scanner(System.in);
+
+        // creating the better look of the system 
+        String name;
+        int age;
+
+        System.out.println("Enter your name: ");
+        name = input.nextLine();
+
+        // creating the input of the coding
+        System.out.println("Enter your age: ");
+        age = input.nextInt();
+
+        // creating the printing the 
+        system.out.println("My name is " + name);
+
+        // to get the value of the age
+        System.out.printf("Your age is %d \n", age);
+
+        
+
     }
 }
